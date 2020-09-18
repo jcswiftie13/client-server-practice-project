3049adf3f30e343c67988e70431a6485c9f29609
JC <ndob.chiu@gmail.com>
2020-09-18 17:18:19 +0800

    加入專案檔案。

---
+++ Client-Server practice project/Server.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SocketListener
+{
+    private static Dictionary<string, int> storage = new Dictionary<string, int>();
+    public static int Main(String[] args)
+    {
+        StartServer();
+        return 0;
+    }
+    private static void SetDic(string name, string value)
+    {
+        storage.Add(name, Convert.ToInt32(value));
+    }
+    public static void StartServer()
+    {
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, 11000);
+        try
+        {  
+            Socket listener = new Socket(IPAddress.Any.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            Console.WriteLine("Waiting for connection...");
+            Socket handler = listener.Accept();
+
+            while (true)
+            {
+                string data = null;
+                string[] command = null;
+                byte[] bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                command = data.Split(" ");
+                if(command[0].ToLower() == "set")
+                {
+                    try
+                    {
+                        SetDic(command[1], command[2]);
+                        byte[] msg = Encoding.ASCII.GetBytes($"SET {command[1]} OK");
+                        int bytesSent = handler.Send(msg);
+                    }
+                    catch(Exception e)
+                    {
+                        byte[] msg = Encoding.ASCII.GetBytes(e.ToString());
+                        int bytesSent = handler.Send(msg);
+                    }
+                }
+                else if(command[0].ToLower() == "get")
+                {
+                    try
+                    {
+                        if (storage.ContainsKey(command[1]))
+                        {
+                            byte[] msg = Encoding.ASCII.GetBytes($"GET {command[1]} {storage[command[1]]}");
+                            int bytesSent = handler.Send(msg);
+                        }
+                        else
+                        {
+                            byte[] msg = Encoding.ASCII.GetBytes($"Did not set this name.");
+                            int bytesSent = handler.Send(msg);
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        byte[] msg = Encoding.ASCII.GetBytes(e.ToString());
+                        int bytesSent = handler.Send(msg);
+                    }
+                }
+                else if((command[0].ToLower() != "quit()") && (command[0].ToLower() != "ping"))
+                {
+                    byte[] msg = Encoding.ASCII.GetBytes($"Invalid input \"{command[0]}\"...please reenter your command.");
+                    int bytesSent = handler.Send(msg);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\n Press any key to continue...");
+        Console.ReadKey();
+    }
+}
---
+++ Client/Client.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Net.NetworkInformation;
+
+public class SocketClient
+{
+    public static int Main(String[] args)
+    {
+        StartClient();
+        return 0;
+    }
+
+
+    public static void StartClient()
+    {
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddress = host.AddressList[1];
+        IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+        Socket sender = new Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp);
+        sender.ReceiveTimeout = 1000;
+
+        try
+        {
+            sender.Connect(remoteEP);
+            Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                string data = null;
+                Console.Write("Input: ");
+                string input = Console.ReadLine();
+                if (input == "PING")
+                {
+                    using (var ping = new Ping())
+                    {
+                        var pingReply = ping.Send(ipAddress);
+
+                        if (pingReply.Status == IPStatus.Success)
+                        {
+                            Console.WriteLine("Ping to " + "[" + ipAddress + "]" + " Successful"
+                   + " Response delay = " + pingReply.RoundtripTime.ToString() + " ms" + "\n");
+                        }
+                        else
+                        {
+                            Console.WriteLine(pingReply.Status);
+                        }
+                        ((IDisposable)ping).Dispose();
+                    }
+                    continue;
+                }
+                //輸入指令, name和value
+                byte[] msg = Encoding.ASCII.GetBytes(input);
+                int bytesSent = sender.Send(msg);
+                //接受回傳訊息
+                int bytesRec = sender.Receive(bytes);
+                data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                Console.WriteLine(data);
+                if (input == "quit()")
+                {
+                    break;
+                }
+            }
+            
+            // Release the socket.    
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+
+        }
+        catch (ArgumentNullException ane)
+        {
+            Console.WriteLine(ane.ToString());
+        }
+
+    }
+}
